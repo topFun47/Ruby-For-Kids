f98afb2c69ccf62536760034bc0ccde9e9ea9cdd
topFun47 <pavel.pstepanov@gmail.com>
2017-05-22 15:35:30 -0400

    Delete Quit.rb

--- Part II/Project 5/Quit.rb
+++
@@ -1,8 +0,0 @@
-#
-# Extra Credit: Simple Adventure
-# Programmed By: Chris Haupt
-# A Random text adventure game.
-# Code Tested By: Pavel Stepanov
-# Assignment: Add a quit command (Q) to the menu and make
-#             the main game loop stop running when it's used.
-#
