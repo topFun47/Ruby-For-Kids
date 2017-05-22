c840a86f621053a791992ab9e61d35b0aa7bcbbd
topFun47 <pavel.pstepanov@gmail.com>
2017-05-22 15:35:05 -0400

    Delete Play.rb

--- Part II/Project 5/Play.rb
+++
@@ -1,9 +0,0 @@
-#
-# Extra Credit: Simple Adventure
-# Programmed By: Chris Haupt
-# A Random text adventure game.
-# Code Tested By: Pavel Stepanov
-# Assignment: Play with the different settings for the game rules.
-#               Does the game get more or less fun if you change how
-#               often the monster attacks or how easy it is to find treasure?
-#
