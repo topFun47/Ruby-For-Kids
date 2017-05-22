500ea5237782a41284c6caf2aab44317e5c5e03e
topFun47 <pavel.pstepanov@gmail.com>
2017-05-22 15:31:02 -0400

    Project 4

---
+++ Part II/Project 4/TriangleTree.rb
@@ -0,0 +1,48 @@
+#
+# Ruby For Kids Project 4: Shapes
+# Programmed By: Chris Haupt
+# Experiment with drawing ASCII art shapes using code.
+# Code Tested By: Pavel Stepanov
+# Extra Credit: Draw three triangles, one on top another
+#               using a loop.
+# Thursday May 18th
+#
+
+def triangle(height, outside_letter, inside_letter)
+  1.upto(height) do |row|
+    print ' ' * (height - row)
+if row == 1
+  puts "#{outside_letter * 2}"
+elsif row == height
+  puts outside_letter * height * 2
+else
+  middle = inside_letter * (row - 2)
+  print "#{outside_letter}#{middle}#{inside_letter}"
+  puts "#{inside_letter}#{middle}#{outside_letter}"
+end
+end
+end
+
+def tree(height, outside_letter, inside_letter)
+  1.upto(3) do |count|
+    puts triangle(height, outside_letter, inside_letter)[count]
+  end
+end
+
+
+
+
+puts "     Welcome to shapes"
+print "Enter your shape size: "
+shape_size = gets
+shape_size = shape_size.chomp
+print "Enter your outside leter: "
+outside_letter = gets
+outside_letter = outside_letter.chomp
+print "Enter your inside letter: "
+inside_letter = gets
+inside_letter = inside_letter.chomp
+
+height = shape_size.to_i
+
+tree(height, outside_letter, inside_letter)
---
+++ Part II/Project 4/UpsidedownTriangle.rb
@@ -0,0 +1,48 @@
+#
+# Ruby For Kids Project 4: Shapes
+# Programmed By: Chris Haupt
+# Experiment with drawing ASCII art shapes using code.
+# Code Tested By: Pavel Stepanov
+# Extra Credit: Create a method called flipped_traingle
+#               and have it draw with the pointed end down.
+#Thursday May 18th
+#
+
+def flipped_triangle(outside_letter, inside_letter, height)
+  height.downto(1) do |row|
+    print ' ' * (height - row)
+    if
+      row == 1
+      puts outside_letter * 2
+    elsif
+      row == height
+      puts outside_letter * height * 2
+    else
+      middle = inside_letter * (row - 2)
+      print "#{outside_letter}#{middle}#{inside_letter}"
+      puts "#{inside_letter}#{middle}#{outside_letter}"
+    end
+  end
+end
+
+
+
+
+puts "    Welcome to shapes"
+print " Please enter your shape size "
+shape_size = gets
+shape_size = shape_size.chomp
+print " Please enter your outside letter "
+outside_letter = gets
+outside_letter = outside_letter.chomp
+print "Please enter your inside letter "
+inside_letter = gets
+inside_letter = inside_letter.chomp
+
+puts "Your shape size will be #{shape_size} big"
+puts "Your outside letter will be #{outside_letter}"
+puts "Your inside letter will be #{inside_letter}"
+
+height = shape_size.to_i
+
+flipped_triangle(outside_letter, inside_letter, height)
