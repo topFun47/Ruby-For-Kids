73ff60a169fb800d49ab4f86a69d0466a7bfeeb6
topFun47 <pavel.pstepanov@gmail.com>
2017-05-22 15:34:46 -0400

    Delete Feature.rb

--- Part II/Project 5/Feature.rb
+++
@@ -1,8 +0,0 @@
-#
-# Extra Credit: Simple Adventure
-# Programmed By: Chris Haupt
-# A Random text adventure game.
-# Code Tested By: Pavel Stepanov
-# Assignment: Come up with a different action the player could take
-#             and add that feature to the game.
-#
