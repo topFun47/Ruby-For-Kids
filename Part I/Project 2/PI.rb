9424086c526d34d9b923de77f82a642964b421ef
topFun47 <pavel.pstepanov@gmail.com>
2017-05-22 15:25:22 -0400

    Project 2

---
+++ Part I/Project 2/PI.rb
@@ -0,0 +1,16 @@
+#
+# Ruby For Kids Project 2: Big Numbers
+# Programmed By: Chris Haupt
+#   Using a combination of your terminal program
+# and Interactive Ruby to do quick experiments
+# that let you try new things with Ruby.
+# Code Tested By: Pavel Stepanov
+# Extra Credit: Use Ruby to calculate the
+#               circumference of the Earth.
+#
+
+PI = Math::PI
+r = 3959
+circumference = 2 * PI * r
+
+puts " The circumference of the earh is #{circumference}mi"
