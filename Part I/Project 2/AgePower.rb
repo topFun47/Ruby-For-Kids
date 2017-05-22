50873561cfceaa9ebe4296a1826ed60f2ec399f5
topFun47 <pavel.pstepanov@gmail.com>
2017-05-22 14:56:23 -0400

    Project 2

---
+++ Part I/Project 2/AgePower.rb
@@ -0,0 +1,11 @@
+#
+# Ruby For Kids Project 2: Big Numbers
+# Programmed By: Chris Haupt
+#   Using a combination of your terminal program
+# and Interactive Ruby to do quick experiments
+# that let you try new things with Ruby.
+# Code Tested By: Pavel Stepanov
+# Extra Credit: What is your age raised by the power of 10?
+#
+
+puts "my age raised by the power of 10 is #{24**10}"
---
+++ Part I/Project 2/NegativeDecimals.rb
@@ -0,0 +1,29 @@
+#
+# Ruby For Kids Project 2: Big Numbers
+# Programmed By: Chris Haupt
+#   Using a combination of your terminal program
+# and Interactive Ruby to do quick experiments
+# that let you try new things with Ruby.
+# Code Tested By: Pavel Stepanov
+# Extra Credit: Can Ruby store negative numbers?
+#               What about decimals?
+#
+
+puts "Can Ruby store negative numbers?"
+puts " How about decimals?"
+puts " "
+puts " Yes, Ruby can store negative numbers"
+puts "  and decimals"
+puts " "
+puts "As you can see here if I add two negative numbers together"
+puts "  like -5 + -10 Ruby will show #{-5 + -10} as the answer."
+puts " "
+puts "And if I add two decimals together like this 1.25 + 1.25 "
+puts " Ruby will show #{1.25 + 1.25} as the answer. "
+puts " "
+puts "So if I make a = -15, and b = 2.5. Than adding a + b together"
+a = -15
+b = 2.5
+puts " Ruby will show #{a + b} as the answer. Meaning that Ruby"
+puts " stored a negative number and a decimal number as variables,"
+puts " and was able to come up with an answer without error."
