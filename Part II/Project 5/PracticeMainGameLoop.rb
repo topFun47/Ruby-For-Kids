79dcda1fa108cefb9d0f33bf2870bfa0d1e2c181
topFun47 <pavel.pstepanov@gmail.com>
2017-05-25 19:23:30 -0400

    Project 5

---
+++ Part II/Project 5/PracticeMainGameLoop.rb
@@ -0,0 +1,71 @@
+"Creating the Main Game Loop"
+# For the adventure project, you're going to start creating the main game loop first, where the rules and input and output of the game are.
+# Then you'll create small methods to implement the functionality you need to run the game.
+# You'll program as if these methods already exist and use Ruby to help fill in the missing functionality.
+
+"Creating the room description and actions"
+# First, you'll need to let the player know what's going on during that turn and describe what the player can do.
+
+#1. Each turn , the player will have a number of options of what to do on her turn.
+#   You'll use the actions variable to hold these choices in an array and reset the array each time you run through the game engine's loop.
+#   You'll use the actions array to build a little menu for the user shortly.
+#   Put the following code inside the while loop:
+ actions = []
+
+#2. Print
+puts ""
+
+#3. You'll
+puts current_room
+
+#4. Now
+if monster
+  puts
+  actions <<
+end
+
+#5. Finally
+  print
+
+"Responding to player actions"
+# Now it's time to get the player's command choice and have the game respond to it.
+
+#1. Collect
+  player_action = gets
+  if
+    damage_points
+    puts
+    end
+
+#2. The Player
+if player_action
+current_room
+end
+
+#3. If the player
+elsif player_action
+  if
+    puts
+    treasure_count
+  else
+    puts
+  end
+  # when you look for treasure you, might attract another monster!
+  if not
+    monster
+  end
+
+  #4. The last
+elsif
+  if
+    monster
+    puts
+  else
+    puts
+  end
+
+  #5. Handle
+else
+  puts
+end
+puts 
