343240df6297a090ba64c7470873d569dfa1fc2e
topFun47 <pavel.pstepanov@gmail.com>
2017-08-11 09:05:37 -0700

    Update PI.rb

--- Part I/Project 2/PI.rb
+++ Part I/Project 2/PI.rb
@@ -1,12 +1,12 @@
-#
-# Extra Credit: Big Numbers
-# Programmed By Pavel Stepanov
-# Using a combination of your terminal program
-# and Interactive Ruby to do quick experiments
-# that let you try new things with Ruby.
-# Assignment: Use Ruby to calculate the
-#               circumference of the Earth.
-#
+################################################
+# Extra Credit: Big Numbers                    #
+# Programmed By Pavel Stepanov                 #
+# Using a combination of your terminal program #
+# and Interactive Ruby to do quick experiments #
+# that let you try new things with Ruby.       #
+# Assignment: Use Ruby to calculate the        #
+#               circumference of the Earth.    #
+################################################
 
 PI = Math::PI
 r = 3959
