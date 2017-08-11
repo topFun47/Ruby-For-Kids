b6fe72bc6f73505a332b1c61a574fc5a183f6e63
topFun47 <pavel.pstepanov@gmail.com>
2017-08-11 09:22:27 -0700

    Update TotalTen.rb

--- Part I/Project 2/TotalTen.rb
+++ Part I/Project 2/TotalTen.rb
@@ -1,13 +1,13 @@
-#
-# Extra Credit: Big Numbers
-# Programmed By Pavel Stepanov
-# Using a combination of your terminal program
-# and Interactive Ruby to do quick experiments
-# that let you try new things with Ruby.
-# Assignment: What is the total if you add the
-#               first ten counting numbers
-#               (1,2,3,4,5,6,7,8,9,10)
-#
+# # # # # # # # # # # # # # # # # # # # # # # # #
+# Extra Credit: Big Numbers                     #
+# Programmed By Pavel Stepanov                  #
+# Using a combination of your terminal program  #
+# and Interactive Ruby to do quick experiments  #
+# that let you try new things with Ruby.        #
+# Assignment: What is the total if you add the  #
+#               first ten counting numbers      #
+#               (1,2,3,4,5,6,7,8,9,10)          #
+# # # # # # # # # # # # # # # # # # # # # # # # #
 puts " "
 puts " If you add the first ten counting numbers, "
 puts "   (1,2,3,4,5,6,7,8,9,and 10)."
