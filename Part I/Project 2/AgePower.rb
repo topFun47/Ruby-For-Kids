c7bc5c260ea1485a2b10a3a9d6f4d6c59eb4b806
topFun47 <pavel.pstepanov@gmail.com>
2017-08-11 09:01:24 -0700

    Update AgePower.rb

--- Part I/Project 2/AgePower.rb
+++ Part I/Project 2/AgePower.rb
@@ -1,10 +1,10 @@
-#
-# Extra Credit: Big Numbers
-# Programmed By Pavel Stepanov
-# Using a combination of your terminal program
-# and Interactive Ruby to do quick experiments
-# that let you try new things with Ruby.
-# Assignment: What is your age raised by the power of 10?
-#
+##########################################################
+# Extra Credit: Big Numbers                              #
+# Programmed By Pavel Stepanov                           #
+# Using a combination of your terminal program           #
+# and Interactive Ruby to do quick experiments           #
+# that let you try new things with Ruby.                 #
+# Assignment: What is your age raised by the power of 10?#
+##########################################################
 
 puts "My age raised by the power of 10 is #{24**10}"
