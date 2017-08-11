5c685810a07bec4f9612d9a7f17bdb1ab021dc7c
topFun47 <pavel.pstepanov@gmail.com>
2017-08-11 09:10:18 -0700

    Update InstallationProcess.rb

--- Part I/Project 1/InstallationProcess.rb
+++ Part I/Project 1/InstallationProcess.rb
@@ -1,7 +1,7 @@
-############################################# 
+# # # # # # # # # # # # # # # # # # # # # # #  
 # Assignment: Install                       #
 # Programmed By Pavel Stepanov              #
 # Instructions for installing Atom and Ruby #
-#############################################
+# # # # # # # # # # # # # # # # # # # # # # # 
 
 #Steps to install go here
