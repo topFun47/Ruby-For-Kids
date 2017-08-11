8d45635858e6494fb8f73cf270d0432f04a1f466
topFun47 <pavel.pstepanov@gmail.com>
2017-08-11 09:02:19 -0700

    Update ConvertDegrees.rb

--- Part I/Project 2/ConvertDegrees.rb
+++ Part I/Project 2/ConvertDegrees.rb
@@ -1,12 +1,12 @@
-#
-# Extra Credit: Big Numbers
-# Programmed By Pavel Stepanov
-# Using a combination of your terminal program
-# and Interactive Ruby to do quick experiments
-# that let you try new things with Ruby.
-# Assignment: Try changing the temperature calculation
-#               to convert degrees Celsius to Fahrenheit.
-#
+##########################################################
+# Extra Credit: Big Numbers                              #
+# Programmed By Pavel Stepanov                           #
+# Using a combination of your terminal program           #
+# and Interactive Ruby to do quick experiments           #
+# that let you try new things with Ruby.                 #
+# Assignment: Try changing the temperature calculation   #
+#               to convert degrees Celsius to Fahrenheit.#
+##########################################################
 
 puts "Try changing the temperature calculation to convert "
 puts " degrees Celsius to Fahrenheit"
