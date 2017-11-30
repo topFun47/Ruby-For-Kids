8ada0255c40c6acf6e0ef260596759b412df4131
topFun47 <pavel.pstepanov@gmail.com>
2017-11-29 17:43:28 -0800

    Update InstallationProcess.rb

--- Part I/Project 1/InstallationProcess.rb
+++ Part I/Project 1/InstallationProcess.rb
@@ -5,3 +5,18 @@
 # # # # # # # # # # # # # # # # # # # # # # # 
 
 #Steps to install go here
+
+Step 1
+# Go to this website https://rubyinstaller.org/downloads/ 
+
+Step 2 
+# Download Ruby 2.3.3 And check boxes — Add Ruby Executables to Your PATH Associate
+#                                     — .rb and .rbw Files with This Ruby Installation
+# Download DEVELOPMENT KIT (32bits version only) — And Extract to C:/DevKit
+
+Step 3 
+# Open Command Prompt
+# Enter cd \devkit
+# Enter ruby dk.rb init
+# Enter ruby dk.rb install
+# Enter gem install gosu
