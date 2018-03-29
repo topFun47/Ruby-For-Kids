c594197e36caf0d1606497627908de3ea82b69d8
topFun47 <pavel.pstepanov@gmail.com>
2018-03-28 23:28:07 -0700

    Add files via upload

---
+++ Part IV/Project 12/Game Files/life.rb
@@ -0,0 +1,35 @@
+# # # # # # # # # # # # # # # # # # # # # # # # #
+# Ruby For Kids Project 12: Life                #
+# Programmed By: Pavel  Stepanov                #
+# A graphical version of Conway's Game of Life  #
+# Started:  Wednesday Mar 28th 2018             #
+# Ended:                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+require 'gosu'
+require_relative 'game'
+
+class Life < Gosu::Window
+def initialize(generations, sim)
+super(800, 800)
+self.caption = "Game of Life"
+@game = Game.new(self, generations, sim)
+end
+
+def update
+@game.update
+end
+def draw
+@game.draw
+end
+end
+
+puts "Welcome to the Game of Life"
+print "How many generations? (0 for infinite) "
+generations = gets.to_i
+print "Pick a simulation (1-5) "
+sim = gets.to_i
+
+window = Life.new(generations, sim)
+window.show
---
+++ Part IV/Project 12/README.txt
@@ -0,0 +1,7 @@
+# # # # # # # # # # # # # # # # # # # # # # # # #
+# Ruby For Kids Project 12: Life                #
+# Programmed By: Pavel  Stepanov                #
+# A graphical version of Conway's Game of Life  #
+# Started:  Wednesday Mar 28th 2018             #
+# Ended:                                        #
+# # # # # # # # # # # # # # # # # # # # # # # # #
